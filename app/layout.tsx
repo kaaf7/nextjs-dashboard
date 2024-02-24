f4a92047752459adaf779f68acea07247b3c8665
Karim <108280260+kaaf7@users.noreply.github.com>
2024-02-24 22:22:42 +0100

    Reformat layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
 import '@/app/ui/global.css';
 import { notinoSans } from '@/app/ui/fonts';
+import SideNav from '@/app/ui/dashboard/sidenav';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode;}) 
+{
   return (
     <html lang="en">
       <body className={`${notinoSans.className} antialiased`}>{children}</body>
