269f746245b1a12949f5ca4d84c56eefe3da4988
Karim <108280260+kaaf7@users.noreply.github.com>
2024-02-24 20:38:39 +0100

    Import new fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,15 @@
-import { Josefin_Sans } from 'next/font/google';
-export const josefinSans = Josefin_Sans({ subsets: ['latin'] });
+import {
+  Noto_Sans,
+  Josefin_Sans,
+  Nunito_Sans,
+  PT_Sans,
+  Lusitana,
+} from 'next/font/google';
+
+export const notinoSans = Nunito_Sans({
+  weight: '300',
+  subsets: ['latin-ext'],
+});
+export const notoSans = Noto_Sans({ weight: '300', subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
