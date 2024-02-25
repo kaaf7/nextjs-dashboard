5366536e96883f879c66f1fc528091e20df28fb7
Karim <108280260+kaaf7@users.noreply.github.com>
2024-02-25 21:17:03 +0100

    Remove promises

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -22,7 +22,6 @@ export async function fetchRevenue() {
     // Don't do this in production :)
 
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<Revenue>`SELECT * FROM revenue`;
     console.log('Data fetch completed after 3 seconds.');
 
@@ -43,7 +42,6 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    await new Promise((resolve) => setTimeout(resolve, 5000));
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
